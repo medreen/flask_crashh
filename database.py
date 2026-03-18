772391c026a5f744734c229dcc0ff7f0805a08a6
medreen.wc <medreenwchege@gmail.com>
2026-03-18 12:21:44 +0300

    authentication

--- database.py
+++ database.py
@@ -3,7 +3,8 @@
 #Define the table structure using classes and objects.
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, DateTime
+# from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
@@ -15,5 +16,13 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(100))
     location: Mapped[str] = mapped_column(String(100))
     age: Mapped[int] = mapped_column(Integer)
+
+class Authentication(Base):
+    __tablename__ = "user_authentication"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    fullname: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(100), nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime)
 
     
--- main.py
+++ main.py
@@ -8,14 +8,15 @@
 from flask import Flask, request, jsonify
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
-from database import Base, User
+from database import Base, User, Authentication
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
 
 allowed_methods = ["GET", "PUT", "POST", "PATCH", "DELETE"]
-DATABASE_URL = "postgresql+psycopg2://postgres:Colesprouse2311!@localhost:5432/flask_crash"
+DATABASE_URL = "postgresql+psycopg2://postgres:Colesprouse2311!@localhost:5432/flask_auth"
 
 # Connecting sqlalchemy to postgresql using engine function_
 engine = create_engine(DATABASE_URL, echo=False)
@@ -26,7 +27,6 @@ my_session = session()
 
 #Create tables automatically
 Base.metadata.create_all(engine)
-
 
 @app.route("/", methods = allowed_methods)
 def home():
@@ -61,6 +61,50 @@ def get_users():
             return jsonify({"msg": f"Successfully added user {data['name']}"})
     else:
         return jsonify({"error": "Method not Allowed"}), 404
+    
+@app.route("/register", methods = allowed_methods)
+def register_user():
+    data = []
+    if request.method.upper() == "POST":       
+        data = request.get_json()
+                  
+        existing_user = my_session.query(Authentication).filter_by(email=data["email"]).first()
 
+        if data["fullname"] == "" or data["email"] == "" or data["password"] == "":
+            return jsonify({"err": "All fields required"}, 400)
+        elif existing_user:
+            return jsonify({"err": "Account already exists"}, 400)
+        else:
+             # create auth record
+            new_auth = Authentication(
+                fullname = data["fullname"],
+                email = data["email"],
+                hashed_password = data["password"],
+                created_at = datetime.utcnow()
+            )   
+            my_session.add(new_auth)
+            my_session.commit()
+            return jsonify({"msg": "User created"}, 201)
+    else:
+        return jsonify({"err": "Method not allowed"})            
+   
 
+@app.route("/login", methods = allowed_methods)
+def login():
+    data = []
+    if request.method.upper() == "POST":
+        data = request.get_json()
+
+        query = select(Authentication).where(Authentication.email == data["email"], Authentication.hashed_password == data["password"])
+        auth = my_session.scalars(query).first()
+        
+        if data["email"] == "" or data["password"] == "":
+            return jsonify({"err": "All fields required"}, 400)
+        elif not auth:
+            return jsonify({"err": "Kindly confirm fields entered"}, 400)
+        else:
+            return jsonify({"msg": "Login successful!"}, 200)
+    else:
+        return jsonify({"msg": "Method not allowed"})
+    
 app.run(debug=True)
